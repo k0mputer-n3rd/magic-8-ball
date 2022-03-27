a7c90bf40e9009b7a4b405b07907337d6fe42d15
k0mputer n3rd <k0mputer-n3rd@outlook.com>
2022-03-27 08:01:09 -0700

    Most bugs fixed

--- js/main.js
+++ js/main.js
@@ -2,17 +2,35 @@
 /* Final Project */
 /* Magic 8-Ball */
 /* Allen P */
-/* 03/25/2022 */
+/* (K0mputer N3rd) */
+/* 03/27/2022 */
 
 /* main.js */
 
+// Your final project will be to build a magic 8 ball app
+// Create a new repository named: magic8ball
+// Clone your repository
+// Add your index file, stylesheet, script file and the provided 8ball images
+// Main objectives:
+// Create an input field for users to ask a question
+// Create a label for your input field telling your user to "Ask me anything!"
+// Create an "ask" button (this will run the function that outputs a random magic 8 ball image)
+// Create a div with an id of "answers" that you will insert the images into using js
+// Create an "ask" function that when clicked, will choose a random 8ball image and insert it into your "answers" div
+// Replace the input field with the user's question when displaying the answer
+// Style your page any way you like
+// Activate Github Pages for your project
+
+// I added more challenges to this:
+// Do the entire project in JavaScript.
+// Handle mobile-friendly scaling (portrait and landscape).
+// HTML will link the JavaScript and have an empty body.
+
 // Bug List:
-// Doesn't handle scroll bar on desktop screen.
 // Add vertical spacing to form.
+// Adjust width of question text box.
 // Add line breaks to footer on narrow screens.
-// Spinner needs to be finished. Maybe one full spin, then stop?
-// Handle return after question is entered, defaultbutton?
-// Eliminate display of previous answers.
+// Remove console.log statements.
 // Do I need to force display refresh for it to look right?
 
 /******\
@@ -55,11 +73,13 @@ var dispHeight = window.innerHeight;
 window.addEventListener("resize", dispOnResize);
 
 // Define variables for HTML Objects
-var div1, div2, footer1, form1, question1, button1, image1;
+var div1, div2, footer1, form1, title1, label1, question1, button1, image1;
+var break1, break2, break3;
+
 initHTML(); // Initialize HTML Body
 resizeHTML(); // Handle Initial Size
 
-// Set an event for the button
+// Set an Event Listener for the button
 button1.addEventListener("click", ask8Ball);
 
 var prevAsk = ""; // Previous question asked (don't ask twice)
@@ -86,9 +106,9 @@ function dispOnResize() {
 function initHTML() {
   // Written as separate expressions for clarity
   let innerHTML = `<div id="div1" style="text-align: center">`;
-  innerHTML += `<h1>Magic 8-Ball</h1>`;
-  innerHTML += `<form id="form1">`;
-  innerHTML += `<label for="question">Ask me anything!</label>`;
+  innerHTML += `<h1 id="title1">Magic 8-Ball</h1>`;
+  innerHTML += `<form id="form1" onsubmit="event.preventDefault(); ask8Ball();" autocomplete="off">`;
+  innerHTML += `<label id="label1" for="question">Ask me anything!</label>`;
   innerHTML += `<br>`;
   innerHTML += `<input type="text" id="question1"></input>`;
   innerHTML += `<br>`;
@@ -100,9 +120,14 @@ function initHTML() {
   innerHTML += `</div>`;
   innerHTML += `<footer id="footer1" style="text-align: center">`;
   innerHTML += `<p>`;
-  innerHTML += `Website Design and Content Copyright &copy; 2022 K0mputer N3rd`;
+  innerHTML += `Website Design and Content `;
+  innerHTML += `<br id="break1">`;
+  innerHTML += `Copyright &copy; 2022 K0mputer N3rd`;
   innerHTML += `<br>`;
-  innerHTML += `Images supplied by BitWise Industries, with some edits by K0mputer N3rd`;
+  innerHTML += `<br id="break2">`;
+  innerHTML += `Images supplied by BitWise Industries `;
+  innerHTML += `<br id="break3">`;
+  innerHTML += `with some edits by K0mputer N3rd`;
   innerHTML += `</p>`;
   innerHTML += `</footer>`;
   document.body.innerHTML = innerHTML;
@@ -113,11 +138,14 @@ function initHTML() {
   div2 = document.getElementById("div2");
   footer1 = document.getElementById("footer1");
   form1 = document.getElementById("form1");
+  title1 = document.getElementById("title1");
+  label1 = document.getElementById("label1");
   question1 = document.getElementById("question1");
   button1 = document.getElementById("button1");
   image1 = document.getElementById("image1");
-
-  console.log("initHTML");
+  break1 = document.getElementById("break1");
+  break2 = document.getElementById("break2");
+  break3 = document.getElementById("break3");
 }
 
 /************\
@@ -127,43 +155,55 @@ function initHTML() {
 // Resize HTML Objects based on Screen Size
 
 function resizeHTML() {
+  var div1Width, div1Float, div2Width, div2Float;
+  var imagePad, imageWidth, textMargin, breakDisplay;
+
   // If Landscape (Width > Height)
   if (dispWidth > dispHeight) {
-    let div1Width = Math.floor(dispWidth * 0.4);
-    let div2Width = Math.floor(dispWidth * 0.6);
-    let imagePad = Math.floor(dispWidth * 0.02);
-    if (dispHeight > div2Width) {
-      imageWidth = div2Width;
-    } else {
-      imageWidth = dispHeight;
-    }
+    div1Width = Math.floor(dispWidth * 0.4);
+    div1Float = "left";
+    div2Width = Math.floor(dispWidth * 0.6) - 17; // Allow for width of scroll bar
+    div2Float = "right";
+    imagePad = Math.floor(dispWidth * 0.02);
+    imageWidth = dispHeight > div2Width ? div2Width : dispHeight;
     imageWidth -= 2 * imagePad;
-    div1.style.width = `${div1Width}px`;
-    div1.style.float = "left";
-    div2.style.width = `${div2Width}px`;
-    div2.style.float = "right";
-    image1.style.padding = `${imagePad}px`;
-    image1.style.width = `${imageWidth}px`;
-
-    footer1.style.clear = "both";
-    console.log(div1.style.width, div2.style.width);
-    console.dir(footer1);
+    textMargin = Math.floor((imageWidth - 100) / 5);
+    textMargin = textMargin < 2 ? 2 : textMargin;
 
     // Else Portrait (Width <= Height)
   } else {
-    let div1Width = Math.floor(dispWidth);
-    let div2Width = Math.floor(dispWidth);
-    let imagePad = Math.floor(dispWidth * 0.02);
+    div1Width = Math.floor(dispWidth);
+    div1Float = "none";
+    div2Width = Math.floor(dispWidth);
+    div2Float = "none";
+    imagePad = Math.floor(dispWidth * 0.02);
     imageWidth = dispWidth - 2 * imagePad;
-    div1.style.width = `${div1Width}px`;
-    div1.style.float = "none";
-    div2.style.width = `${div2Width}px`;
-    div2.style.float = "none";
-    image1.style.padding = `${imagePad}px`;
-    image1.style.width = `${imageWidth}px`;
+    textMargin = Math.floor((dispHeight - imageWidth - 100) / 5);
+    textMargin = textMargin < 2 ? 2 : textMargin;
   }
+  
+  // Handle Breaks for Narrow Footer
+  breakDisplay = ((dispWidth > 500) ? "none" : "inherit");
 
-  console.log("resizeHTML", dispWidth, dispHeight);
+  // Set Style Values
+  div1.style.width = `${div1Width}px`;
+  div1.style.float = div1Float;
+  div2.style.width = `${div2Width}px`;
+  div2.style.float = div2Float;
+  title1.style.marginTop = `${textMargin}px`;
+  title1.style.marginBottom = `${textMargin}px`;
+  question1.style.marginTop = `${textMargin}px`;
+  button1.style.marginTop = `${textMargin}px`;
+  button1.style.marginBottom = `${textMargin}px`;
+  image1.style.padding = `${imagePad}px`;
+  image1.style.width = `${imageWidth}px`;
+
+  footer1.style.width =`${dispWidth}px`
+  footer1.style.float = "none";
+  footer1.style.clear = "both";
+  break1.style.display = breakDisplay;
+  break2.style.display = breakDisplay;
+  break3.style.display = breakDisplay;
 }
 
 /**********\
@@ -183,6 +223,8 @@ function ask8Ball() {
     return;
   }
   prevAsk = questionAsk; // Save Answer
+  question1.value = "";
+  question1.placeholder = prevAsk;
   let imgIndex = Math.trunc(Math.random() * imgArray.length);
   if (imgIndex === prevImg) {
     imgIndex++;
@@ -191,7 +233,6 @@ function ask8Ball() {
     }
   }
   prevImg = imgIndex;
-  console.log(imgIndex);
   spinAnswer(imgIndex);
 }
 
@@ -205,5 +246,5 @@ function spinAnswer(index) {
   image1.src = imgSpin;
   setTimeout(function () {
     image1.src = imgArray[index];
-  }, 4000);
+  }, 3000);
 }
